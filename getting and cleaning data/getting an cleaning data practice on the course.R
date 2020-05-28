247437f836440db71996726d40d411c58ef46c62
lordasd <asd_vas@yahoo.com>
2020-05-28 05:40:14 +0200

    soving the test
    soving the test

---
+++ getting and cleaning data/getting an cleaning data practice on the course.R
@@ -0,0 +1,32 @@
+#getting an cleaning data practice on the course 
+
+## downloading files 
+
+#get and set WD
+
+## ../ up and 
+## 
+
+
+## use the backslah  for windows 
+
+if (!file.exists('the file'))
+{
+    dir.create('data')
+}
+
+#url = 
+#distination = 
+#method =
+
+url <-  
+distination <- 
+
+    
+path <- getwd()
+
+#to reach some path use the file.path function 
+file.path(path, "UCI HAR Dataset/train/Y_train.txt")
+
+download.file(url, distfile = distination, method = "crul")
+
