f674850fa1818fe1488f2a3af8429b073abbe8eb
lordasd <asd_vas@yahoo.com>
2020-06-16 07:29:26 +0200

    R file frist tries
    R file frist tries

---
+++ Exploratory Data Analysis/general course practice.R
@@ -0,0 +1,35 @@
+##
+rstudioapi::getActiveDocumentContext
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+getwd()
+
+pollution <- read.csv("data/avgpm25.csv", colClasses = c("numeric", "character", 
+                                                         "factor", "numeric", "numeric"))
+head(pollution)
+
+summary(pollution$pm25)
+
+boxplot(pollution$pm25, col = "blue")
+
+hist(pollution$pm25, col = "green")
+
+hist(pollution$pm25, col = "green")
+rug(pollution$pm25)
+
+hist(pollution$pm25, col = "blue", breaks = 100)
+rug(pollution$pm25)
+
+
+boxplot(pollution$pm25, col = "blue")
+abline(h = 12)
+
+barplot(table(pollution$region) , main = "test")
+
+
+boxplot(pm25 ~ region, data = pollution)
+
+par(mfrow = c(2, 1) , mar = c(4,4,2,1))
+hist(subset(pollution, region == "east")$pm25, col = "green")
+hist(subset(pollution, region == "west")$pm25, col = "green")
+
+
